76da26191c8687052d05a97ba94235c6a71f44a4
Shalvi Kubal <shalvi_kubal@persistent.co.in>
2019-02-22 15:24:51 +0530

    file Home added

---
+++ university/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+           In Home
+        </div>
+    );
+  }
+}
+
+export default Home;
